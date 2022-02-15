8942066e76a21a346b2f612fbf9d83b8dbe5bbe2
RaquelArzaluzLuna <raquel.arzaluzluna@gmail.com>
2022-02-14 21:23:13 -0600

    first commit

---
+++ katas _semana1/kata01/kata1.py
@@ -0,0 +1,12 @@
+
+from datetime import date
+date.today()
+
+print("Today's date is: " + str(date.today()))
+
+
+
+parsec = 11
+lightyears = 3.26156 * parsec
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
